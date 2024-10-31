aee85a1f58d4a0ecc9dff34f4f6d64c2fb13c74a
utumbo <121032782+utumbo@users.noreply.github.com>
2024-10-31 22:46:16 +0700

    version 1.1
    New function

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import cv2
+import os
 
-#загрузка классификатора
+#загрузка каскада
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-#загрузка изображения
-img = cv2.imread('face.jpg')
+#Папка для фото
+image_folder = 'input_face'
+output_folder = 'output_face'
 
-#преобразование в серый
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#Создание папки для готового фото если её нет
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-#обнаружение лиц
-face = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+#получение  всех файлов в папке
+image_files = [f for f in os.listdir(image_folder) if f.endswith(('.jpg', '.jpeg', '.png'))]
 
-#прямоугольники вокруг лиц
-for (x, y, w, h) in face:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+#Обработка изображений
+for image_file in image_files:
+    img_path = os.path.join(image_folder, image_file)
+    img = cv2.imread(img_path)
 
-#отображение результата
-cv2.imshow('Detected_face', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #Проверка загрузки изображения
+    if img is None:
+        print(f"failed to upload image: {img_path}")
+        continue
+
+    #Преображение в серый
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    #обнаружение лиц
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    #вырезаем и сохранем лица
+    for i, (x, y, w, h) in enumerate(faces):
+        face_img = img[y:y + h, x:x + w] #вырезаем лицо
+        face_filename = os.path.join(output_folder, f"{os.path.splitext(image_file)[0]}_face_{i + 1}.jpg")
+        cv2.imwrite(face_filename, face_img)
+
+    #отображение результата
+    cv2.imshow('Detected_face', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
